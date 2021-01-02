1282d16c2ece1a97cc24a2272ac45ff1c55e0f50
yokotahiroto <hy_dkbs66mh@icloud.com>
2021-01-02 05:12:52 +0000

    [add]admin_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'members/index'
+    get 'members/show'
+    get 'members/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/new'
+    get 'items/index'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
