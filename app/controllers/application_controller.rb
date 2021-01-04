7a11b17b45d655a9bc81dfc480014b1644f42670
manami-m7 <kmuuum@gmail.com>
2021-01-04 05:57:57 +0000

    [fix]routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :kana_last_name, :kana_first_name, :postal_code, :street_address, :phone_number, :is_active])
+  end
 end
