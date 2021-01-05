42f2d8ee1b23ea95797beca4327a65e001f27ccd
lv_jiiiiu <lv.jiiiiu@gmail.com>
2021-01-05 03:57:54 +0000

    [Add]ジャンル一覧画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,14 +1,29 @@
 class Admin::GenresController < ApplicationController
   def index
-  end
-
-  def edit
+    @genres = Genre.all
+    @genre = Genre.new
   end
   
   def create
+    @genres = Genre.all
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to admin_genres_path
   end
+
+
+  def edit
+    
+  end
+  
+  
   
   def update
   end
   
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name, :is_active)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,3 @@
 class Order < ApplicationRecord
+  enum payment_method: {credit_card: 0, bank_transfer: 1}
 end
