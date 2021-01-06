eec288b8831632f6e56b93c21bfa991ca2a30cb4
lv_jiiiiu <lv.jiiiiu@gmail.com>
2021-01-06 04:57:10 +0000

    管理者ログイン機能マージ前

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,8 +1,11 @@
 class Admin::MembersController < ApplicationController
+  
   def index
+    @members = Member.page(params[:page]).reverse_order
   end
 
   def show
+    @member = Member.find(params[:id])
   end
 
   def edit
