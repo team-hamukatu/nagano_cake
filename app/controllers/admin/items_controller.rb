63b323083e420fabbd6e4ee440c5cab7f4651f39
yokotahiroto <hy_dkbs66mh@icloud.com>
2021-01-06 01:42:02 +0000

    test

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,30 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new 
   end
 
   def index
+    @items = Item.all
   end
 
-  def show
+  def show 
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
   
   def create
+    item = Item.new(blog_params)
+    item.save
+    redirect_to admin_item_path(item.id)
   end
   
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)
   end
   
 end
