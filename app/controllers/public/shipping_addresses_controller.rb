9128b2f29232880d01673074696697104241de6c
lv_jiiiiu <lv.jiiiiu@gmail.com>
2021-01-07 12:25:00 +0000

    public-registrationsをpullする前

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,8 +1,14 @@
 class Public::ShippingAddressesController < ApplicationController
   def index
+    @shipping_addresses = ShippingAddress.all
+    @shipping_address = ShippingAddress.new
   end
 
   def create
+    shipping_address = ShippingAddress.new(shipping_addresse_params)
+    shipping_address.member_id = current_member.id
+    shipping_address.save
+    redirect_to shipping_addresses_path
   end
 
   def edit
@@ -12,5 +18,10 @@ class Public::ShippingAddressesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def shipping_addresse_params
+    params.require(:shipping_address).permit(:member_id, :shipping_postal_code, :shipping_street_address, :shipping_name)
   end
 end
