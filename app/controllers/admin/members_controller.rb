6f331829a8cc26eda981686b1743b9afb4f01716
lv_jiiiiu <lv.jiiiiu@gmail.com>
2021-01-08 00:01:53 +0000

    editのデザイン以外完成

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,7 +1,7 @@
 class Admin::MembersController < ApplicationController
-  
+
   def index
-    @members = Member.page(params[:page]).reverse_order
+    @members = Member.page(params[:page])
   end
 
   def show
@@ -11,12 +11,13 @@ class Admin::MembersController < ApplicationController
   def edit
     @member = Member.find(params[:id])
   end
-  
+
   def update
-    member = Member.find(params[:id])
-    member.update(member_params)
+    @member = Member.find(params[:id])
+    @member.update(member_params)
+    redirect_to admin_member_path(@member)
   end
-  
+
   def member_params
     params.require(:member).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :postal_code, :street_address, :phone_number, :email, :is_active)
   end
